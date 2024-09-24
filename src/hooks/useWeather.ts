a8e7ddf07ee63f07f1c2e89e815a6dd1e4374575
Ignacio M Arteaga P <ignacio.moises.arteaga.pelcast@gmail.com>
2024-09-24 09:43:02 -0600

    Production v1

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -38,7 +38,7 @@ export const useWeather = () => {
         setWeather(initialState);
 
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
             const {data} = await axios(geoUrl);
 
